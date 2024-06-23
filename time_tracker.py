a9cea640851d3b0be4168f68be4484e53979aeb9
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-06-23 22:51:31 +0200

    Added core functionality, reading filepath from environment varialbe, can now track times and stop tracking times, everything gets saved into data.json

--- time_tracker.py
+++ time_tracker.py
@@ -1,93 +1,139 @@
-import datetime as dt
+import argparse
+import json
 import os
+import select
+import sys
+import termios
 import time
+import tty
 from typing import Optional
 
-import ujson as json
+SECOND_TO_MS = 1e3
+MS_TO_SECOND = 1e-3
+
+PRINT_DELAY = 0.3
+SAVE_DELAY = 10.0
 
 
 class TimeTracker:
     def __init__(self) -> None:
-        self.local_folderpath = "./time_tracker/data.json"
+        filename = "data.json"
+        self.local_folderpath: str = os.path.join(
+            os.environ.get("DS_TIME_TRACKING_DIR", "./time_tracker/"), filename
+        )
+
+        if not os.path.exists(self.local_folderpath):
+            print(f"Creating {self.local_folderpath}.")
+            os.makedirs(os.path.dirname(self.local_folderpath), exist_ok=True)
+            with open(self.local_folderpath, "w") as f:
+                json.dump({}, f)
 
         with open(self.local_folderpath, "r") as f:
             self.data = json.load(f)
 
-        self.tags = self.data.get("tags", [])
-        self.entries = self.data.get("entries", {})
-
-        self.current_entry = None
-
-        self.is_running = False
-
-    def create_new_entry(self, tags: list[str]) -> None:
-        if any(tag not in self.tags for tag in tags):
-            raise ValueError(f"Tags {tags} not found in {self.tags}")
-
-        timestamp_start = int(time.time() * 1000)
-        current_entry = timestamp_start
-        self.entries[current_entry] = {"tags": tags, "iterations": []}
-
-    def start_entry(self, key: int) -> None:
-        t_curr = int(time.time() * 1000)
-
-        self.entries[key]["iterations"].append({"start": t_curr})
-        self.current_entry = key
-
-    def stop_entry(self, key: Optional[int]) -> None:
-        if key is None:
-            key = self.current_entry
-        else:
-            raise RuntimeError("No entry is currently running and no key was passed.")
-
-        t_curr = int(time.time() * 1000)
-
-        self.entries[key]["iterations"][-1]["stop"] = t_curr
-        self.current_entry = None
-
-    def get_latest_entry(self) -> int:
-        return max(self.entries.keys())
-
-    def update(self) -> None:
+    def save_data(self) -> None:
         with open(self.local_folderpath, "w") as f:
-            json.dump(self.data, f)
+            json.dump(self.data, f, indent=4)
 
-    def run(self) -> None:
-        self.is_running = True
+    def register_new_project(
+        self,
+        project_name: str,
+        tags: Optional[list[str]] = None,
+    ) -> None:
+        if project_name in self.data:
+            print(f"Project {project_name} already exists")
+            return
 
-        print("Starting to run the timetracking utility.")
-        while self.is_running:
-            inp = input(
-                "Add comments or stop with -s or quit with -q or list all entries with -ls.\nAdd a new entry with `-n '[*tags]'.\n"
-            )
+        self.data[project_name] = {
+            "tags": tags or [],
+            "entries": [],
+        }
 
-            if inp == "-s":
-                pass
-            elif inp == "-q":
-                self.is_running = False
-            elif inp == "-ls":
-                print(self.entries)
-            else:
-                raise NotImplementedError("Comments not supported yet.")
+    def start_tracking(
+        self,
+        project_name: str,
+        comment: Optional[str] = None,
+    ) -> None:
+        if project_name not in self.data:
+            print(f"Registering {project_name=} before tracking.")
+            self.register_new_project(project_name)
 
-            print()
+        self.data[project_name]["entries"].append(
+            {
+                "start": int(time.time() * SECOND_TO_MS),
+                "stop": None,
+                "comment": comment or "",
+            }
+        )
 
-            self.update()
+    def stop_tracking(self, project_name: str) -> None:
+        if project_name not in self.data:
+            print(f"Project {project_name} not found")
+            return
 
-        print("Quitting the timetracking utility.")
-        self.update()
+        self.data[project_name]["entries"][-1]["stop"] = int(time.time() * SECOND_TO_MS)
+        self.save_data()
 
-    def get_obsidian_folderpath() -> str:
-        time_tracking_dir: Optional[str] = os.getenv("DS_TIME_TRACKING_DIR")
+    def get_total_time_in_project(self, project_name: str) -> float:
+        total_time_ms = 0
+        for entry in self.data[project_name]["entries"]:
+            if entry["stop"] is not None:
+                total_time_ms += entry["stop"] - entry["start"]
+        return total_time_ms / SECOND_TO_MS
 
-        if time_tracking_dir is None:
-            raise ValueError("DS_TIME_TRACKING_DIR environment variable must be set")
-        return time_tracking_dir
+    def __del__(self) -> None:
+        print("Starting to save the data.")
+        self.save_data()
+        print("Data saved.")
+
+
+def is_data() -> bool:
+    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
 
 def main() -> None:
+    parser = argparse.ArgumentParser(description="Time Tracking Application")
+    parser.add_argument(
+        "-p", "--project", type=str, help="Project name to start tracking"
+    )
+    parser.add_argument(
+        "-c", "--comment", type=str, help="Comment for the tracking entry"
+    )
+    parser.add_argument("-ls", "--list", action="store_true", help="List all data")
+
+    args = parser.parse_args()
+
     time_tracker = TimeTracker()
-    time_tracker.run()
+
+    if args.list:
+        with open(time_tracker.local_folderpath, "r") as f:
+            print(json.dumps(json.load(f), indent=4))
+        return
+
+    project_name = str(args.project if args.project else "NO_NAME")
+    comment = str(args.comment if args.comment else "")
+    time_tracker.start_tracking(project_name, comment)
+    print(f"Started tracking project: {project_name}")
+
+    t0: float = time.time()
+    time_until_next_save = t0 + SAVE_DELAY
+    old_settings = termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin.fileno())
+    try:
+        while not is_data():
+            elapsed_time = time.time() - t0
+            print(f"\r{elapsed_time:7.0f} seconds elapsed.", end="")
+            sys.stdout.flush()
+            time.sleep(PRINT_DELAY)
+            if time.time() > time_until_next_save:
+                assert False
+                time_tracker.save_data()
+                time_until_next_save = time.time() + SAVE_DELAY
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+    time_tracker.stop_tracking(project_name)
+    print("\nTracking stopped.")
 
 
 if __name__ == "__main__":
